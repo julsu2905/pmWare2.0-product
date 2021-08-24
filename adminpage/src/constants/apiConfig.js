6a072853368c8266e13fe742c16909a934287942
Jul <69659361+julsu2905@users.noreply.github.com>
2021-08-24 10:01:12 +0700

    Update apiConfig.js

--- adminpage/src/constants/apiConfig.js
+++ adminpage/src/constants/apiConfig.js
@@ -1,8 +1,11 @@
-export const DOMAIN_CLIENT = "http://localhost:9696/api/";
-export const DOMAIN_ADMIN_CLIENT = "http://localhost:9696/api/adminUsers/";
-export const IMAGES_URL = "http://localhost:9696/img/user/";
-export const AVATAR_URL = "http://localhost:9696/img/user/";
-export const BANNER_URL = "http://localhost:9696/img/banner/";
-export const FILE_URL = "http://localhost:9696/uploads/";
-export const UPLOAD_URL = "http://localhost:9696/api/uploads/image/";
-export const UPLOAD_FILE_URL = "http://localhost:9696/api/uploads/file";
+export const DOMAIN_CLIENT = "https://pmware-server.herokuapp.com/api/";
+export const DOMAIN_ADMIN_CLIENT =
+  "https://pmware-server.herokuapp.com/api/adminUsers/";
+export const IMAGES_URL = "https://pmware-server.herokuapp.com/img/user/";
+export const AVATAR_URL = "https://pmware-server.herokuapp.com/img/user/";
+export const BANNER_URL = "https://pmware-server.herokuapp.com/img/banner/";
+export const FILE_URL = "https://pmware-server.herokuapp.com/uploads/";
+export const UPLOAD_URL =
+  "https://pmware-server.herokuapp.com/api/uploads/image/";
+export const UPLOAD_FILE_URL =
+  "https://pmware-server.herokuapp.com/api/uploads/file";
